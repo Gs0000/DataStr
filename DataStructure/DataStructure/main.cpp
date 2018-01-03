11917103b1561ad4b4bac1cc3844d10440b00eaf
Gs0000 <1006231711@qq.com>
2018-01-03 20:25:11 +0800

    0103

---
+++ DataStructure/DataStructure/main.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+
+using namespace std;
+
+int main() 
+{
+	switch ()
+	{
+	case 0:
+		break;
+	case 1:
+		break;
+	case 2:
+		break;
+	case 3:
+		break;
+	default:
+		break;
+	}
+
+	return 0;
+}
